3e89f84a1266678fbbef92eb03dd0e6007892994
SANGET <AlexZhang@Alexs-iMac.local>
2018-07-05 17:48:20 +0800

    新增一些文件

---
+++ app.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+import UITester from './test';
+
+ReactDOM.render((
+  <UITester/>
+), document.getElementById('Main'));
