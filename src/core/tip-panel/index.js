57791dde1227932e2a80ccde8d4a0609f0de8d78
SANGET <AlexZhang@Alexs-iMac.local>
2018-10-05 14:14:44 +0800

    优化 tipPanel 组件的样式和增强其功能

--- src/core/tip-panel/index.js
+++ src/core/tip-panel/index.js
@@ -1,39 +1,56 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+
 import ToolTip from '../tooltip';
 
 export default class TipPanel extends PureComponent {
   static propTypes = {
     title: PropTypes.string,
     text: PropTypes.string,
+    type: PropTypes.string,
     defaultShow: PropTypes.bool,
     texts: PropTypes.array,
+    needToolTip: PropTypes.bool,
   };
-  constructor(props) {
-    super(props);
-
-    const { defaultShow = true } = props;
-
-    this.state = {
-      showContent: defaultShow
-    };
+  static defaultProps = {
+    type: 'warm',
+    needToolTip: false,
+    defaultShow: true,
+    title: '',
+    text: '',
   }
+  state = {
+    showContent: this.props.defaultShow
+  };
+  // constructor(props) {
+  //   super(props);
+
+  //   const { defaultShow } = props;
+
+  //   this.state = {
+  //     showContent: defaultShow
+  //   };
+  // }
   toggleContent() {
-    this.setState({
-      showContent: !this.state.showContent
-    });
+    this.setState(({showContent}) => ({
+      showContent: !showContent
+    }));
   }
   render() {
-    const {title, text, texts = [], type = 'warm', ...other} = this.props;
+    const {title, text, texts = [], type, needToolTip, ...other} = this.props;
     const {showContent} = this.state;
 
     const titleDOM = title ? (
       <h4 className="title">
         {title}
-        <ToolTip
-          title={showContent ? '缩起': '展开'}
-          type={showContent ? "circle-up" : "circle-down"}
-          onClick={e => this.toggleContent()}/>
+        {
+          needToolTip ? (
+            <ToolTip
+              title={showContent ? '缩起': '展开'}
+              type={showContent ? "circle-up" : "circle-down"}
+              onClick={e => this.toggleContent()}/>
+          ) : null
+        }
       </h4>
     ) : null;
   
@@ -47,7 +64,7 @@ export default class TipPanel extends PureComponent {
       let currT = texts[i];
       if(currT) textsDOM.push((
         <div className="item" key={i}>{i + 1 + '.'} {currT}</div>
-      ))
+      ));
     }
   
     return (
@@ -62,6 +79,6 @@ export default class TipPanel extends PureComponent {
           ) : null
         }
       </div>
-    )
+    );
   }
 } 
