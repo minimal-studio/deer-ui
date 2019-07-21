af8e2171ed028a839f2f3db690b53dce30c12e3d
SANGET <3348592+SANGET@users.noreply.github.com>
2019-07-21 21:19:55 +0800

    修改配置

--- doczrc.js
+++ doczrc.js
@@ -1,17 +1,18 @@
 // import doczPluginNetlify from "docz-plugin-netlify";
 import { css } from 'docz-plugin-css';
+import path from 'path';
 import themeConfig from './docz/theme-config/config';
 
 export default {
   // dest: 'docz-dist',
   title: 'Ukelli-UI',
-  description: 'Ukelli-UI Lib, 轻量级 React UI 框架',
+  description: 'Ukelli-UI, 轻量级 React UI 框架',
   notUseSpecifiers: true,
-  filterComponents: (files) =>
-    files.filter(filepath => /[w-]*.(js|jsx|ts|tsx)$/.test(filepath)),
+  filterComponents: files => files.filter(filepath => /[w-]*.(js|jsx|ts|tsx)$/.test(filepath)),
   indexHtml: 'docz/index.html',
   wrapper: 'docz/wrapper',
-  // theme: 'docz-theme-ztopia',
+  public: path.resolve(__dirname, './public'),
+  // theme: path.resolve(__dirname, './docz/theme/theme.tsx'),
   codeSandbox: false,
   hashRouter: true,
   typescript: true,
@@ -27,27 +28,19 @@ export default {
   },
   themeConfig,
   menu: [
-    'Getting Started / 开始',
-    'Style / 样式',
-    'Layout / 布局',
-    'Utils / 效率',
-    'Config / 配置',
-    'Components / 组件',
-    'Selector / 选择器',
-    'Form / 表单',
-    'FormGenerator / 表单生成',
-    'Table / 表格渲染',
-    'Other / 其他组件',
+    'Getting Started',
+    'Style',
+    'Layout',
+    'Utils',
+    'Components',
+    'Selector',
+    'Form',
+    'FormGenerator',
+    'Data Display',
+    'Other',
+    'Config',
     'More',
   ],
-  // modifyBundlerConfig: (config) => {
-  //   config.resolve.extensions.push('.scss');
-  //   config.module.rules.push({
-  //     test: /\.scss$/,
-  //     use: ["style-loader", "css-loader", "sass-loader"]
-  //   });
-  //   return config;
-  // },
   plugins: [
     // doczPluginNetlify(),
     css({
