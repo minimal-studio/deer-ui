c9388029e8a07bc4043dd152846cc54569585c61
SANGET <3348592+SANGET@users.noreply.github.com>
2019-07-28 13:36:44 +0800

    update Icon

--- src/core/icon/icon.tsx
+++ src/core/icon/icon.tsx
@@ -16,7 +16,7 @@ export interface IconProps {
   /** multiple class names, 例如 ['class1', 'class2'] */
   classNames?: string[];
   /** style */
-  style: {};
+  style?: {};
 }
 
 const Icon: React.SFC<IconProps> = (props) => {
