a07942b03ddb466706320f4dff429cbc3507657d
SANGET <3348592+SANGET@users.noreply.github.com>
2019-08-05 08:21:34 +0800

    style: add logo url from github

--- docz/theme-config/config.js
+++ docz/theme-config/config.js
@@ -22,7 +22,7 @@ export default {
    * Logo
    */
   logo: {
-    src: '/public/logo.png',
+    src: 'https://raw.githubusercontent.com/ukelli/ukelli-ui/master/public/logo.png',
     width: 120
   },
   /**
