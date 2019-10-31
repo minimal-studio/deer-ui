a41f687eae551cfaa89db68a2f262fcb6c59799f
SANGET <zh.sanget@gmail.com>
2019-10-31 16:54:49 +0800

    Update website

--- website/gatsby-ssr.js
+++ website/gatsby-ssr.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { RootWrapper } from '../components/root-wrapper';
+import { LoaderDOM, LoadedDOM } from '../utils/loader-script';
 
 export const wrapRootElement = ({ element }) => {
   return <RootWrapper>{element}</RootWrapper>;
@@ -8,6 +9,10 @@ export const wrapRootElement = ({ element }) => {
 export const onRenderBody = (
   { setPreBodyComponents, setPostBodyComponents },
 ) => {
+  setPreBodyComponents([
+    <LoadedDOM key="1" />,
+    <LoaderDOM key="2" />,
+  ]);
   setPostBodyComponents([
     <link key="1" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.3.1/css/all.min.css" />,
     <link key="2" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/themes/airbnb.css" />
