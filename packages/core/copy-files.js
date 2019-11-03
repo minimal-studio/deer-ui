39adcb111a829601fb90bc0527900110eee47e8e
sanget <zh.sanget@gmail.com>
2019-11-03 22:48:42 +0800

    Finished build env

--- packages/core/copy-files.js
+++ packages/core/copy-files.js
@@ -1,9 +1,10 @@
+const path = require('path');
 const copyFile = require('../../scripts/copy-files');
 
 copyFile({
-  outdir: '../../dist/core',
-  targetFiles: ['./README.md'],
-  targetPackageJson: ['./package.json'],
+  outdir: path.resolve('../../dist/core'),
+  targetFiles: [path.resolve('./README.md')],
+  targetPackageJson: path.resolve('./package.json'),
   packageExtraOptions: {
     types: './index.d.ts'
   }
