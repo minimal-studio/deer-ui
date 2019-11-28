fa824ad2fa09c5ba18f276609e95464a2568cf63
sanget <zh.sanget@gmail.com>
2019-11-28 08:49:32 +0800

    Add UI intro

--- website/src/components/home-page.js
+++ website/src/components/home-page.js
@@ -9,7 +9,7 @@ const Container = styled.div`
 `;
 
 const UIName = styled.div`
-  font-size: 40px;
+  font-size: 30px;
   margin-bottom: 20px;
 `;
 
@@ -20,25 +20,60 @@ const UILogo = styled.div`
 const Desc = styled.div`
   font-size: 18px;
   margin-bottom: 20px;
+  width: 500px;
+  margin: 20px auto;
+  line-height: 28px;
+`;
+
+const MainIntro = styled.div`
+  text-align: center;
+`;
+
+const FeatherIntro = styled.div`
+  margin-top: 50px;
+`;
+
+const FeatherIntroItem = styled.div`
 `;
 
 const HomePage = () => {
   return (
     <Container>
-      <Grid container>
-        <Grid lg={4}>
-          <UILogo>@deer-ui</UILogo>
+      <MainIntro>
+        <UIName>
+          Building Form Group by the easiest way.
+        </UIName>
+        <Desc>
+          Deer ui is a Extendable's UI lib base on React
+          , provide FormGenerator to help us to build form logic clearly
+          , and also provide a useful Table to display data.
+        </Desc>
+        <Link
+          className="btn theme hola"
+          to="/docs/getting-started/">Getting Started</Link>
+      </MainIntro>
+      <FeatherIntro>
+        <Grid container space={10}>
+          <Grid lg={3} sm={6} xs={12}>
+            <FeatherIntroItem>
+              <h4>Typescript support</h4>
+              <div>
+                Base on typescript to construct
+                , totally support code hint and type check, safe to use.
+              </div>
+            </FeatherIntroItem>
+          </Grid>
+          <Grid lg={3} sm={6} xs={12}>
+            <FeatherIntroItem>
+              <h4>Both Desktop and Mobile app support</h4>
+              <div>
+                Take into account the different between Desktop and Mobile web app
+                , automate detect device and choice the right way to render.
+              </div>
+            </FeatherIntroItem>
+          </Grid>
         </Grid>
-        <Grid lg={8}>
-          <UIName>Deer UI</UIName>
-          <Desc>
-            可扩展的 UI 库
-          </Desc>
-          <Link
-            className="btn theme hola"
-            to="/docs/getting-started/">Getting Started</Link>
-        </Grid>
-      </Grid>
+      </FeatherIntro>
     </Container>
   );
 };
