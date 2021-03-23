ffa6a8bafdc2d669f8392bc29047c35959aa99c2
sanget <zh.sanget@gmail.com>
2021-03-23 21:31:37 +0800

    Update env config

--- packages/core/dropdown-wrapper/dropdown-wrapper.tsx
+++ packages/core/dropdown-wrapper/dropdown-wrapper.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import classnames from 'classnames';
-import { IsFunc, DebounceClass } from '@mini-code/base-func';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import React from "react";
+import ReactDOM from "react-dom";
+import classnames from "classnames";
+import { IsFunc, DebounceClass } from "@mini-code/base-func";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
-import { Children, FuncChildren } from '../utils/props';
-import { getElementOffsetInfo } from '../utils/get-elem-offset';
+import { Children, FuncChildren } from "../utils/props";
+import { getElementOffsetInfo } from "../utils/get-elem-offset";
 import {
-  getLeft, getRight, getTop, getBottom, PositionReturn,
-} from '../utils/position';
-import positionFilter from '../utils/position-filter';
-import setDOMById from '../utils/set-dom';
+  getLeft,
+  getRight,
+  getTop,
+  getBottom,
+  PositionReturn,
+} from "../utils/position";
+import positionFilter from "../utils/position-filter";
+import setDOMById from "../utils/set-dom";
 
-import { Icon } from '../icon';
-import { ClickAway } from '../click-away';
-import { queryIsMobile } from '../utils';
+import { Icon } from "../icon";
+import { ClickAway } from "../click-away";
+import { queryIsMobile } from "../utils";
 
 interface State {
   isShow: boolean;
@@ -41,7 +45,7 @@ export interface DropdownWrapperProps {
   /** 弹出的位置，用 , 分隔，最多支持两个不冲突位置，如果冲突，则选择第一个值 */
   position?: string;
   /** className */
-  className?: HTMLElement['className'];
+  className?: HTMLElement["className"];
   /** 外层的 title */
   menuTitle?: string | number | Children;
   /** 接受函数 children，只在 show 的时候渲染 */
@@ -53,22 +57,23 @@ export interface DropdownWrapperProps {
   /** 父容器的 scrollY 偏移值 */
   scrollY?: number;
   /** 监听滚动时隐藏的外层元素 */
-  trigger?: 'click' | 'hover';
+  trigger?: "click" | "hover";
   /** 用于渲染最外层的内容 */
   overlay?: (helper: FuncChildrenParams) => Children;
   /** style */
   style?: React.CSSProperties;
 }
 
-const dropdownContainerID = 'DropdownContainer';
+const dropdownContainerID = "DropdownContainer";
 let dropdownContainerDOM: HTMLElement;
 
 const calculateOverlayPosition = (options) => {
+  const { target, position, overlayElem, scrollX, scrollY } = options;
   const {
-    target, position, overlayElem, scrollX, scrollY
-  } = options;
-  const {
-    offsetTop, offsetLeft, offsetWidth, offsetHeight
+    offsetTop,
+    offsetLeft,
+    offsetWidth,
+    offsetHeight,
   } = getElementOffsetInfo(target);
   const overlayElemWidth = overlayElem.offsetWidth;
   const overlayElemHeight = overlayElem.offsetHeight;
@@ -78,21 +83,21 @@ const calculateOverlayPosition = (options) => {
     elemHeight: overlayElemHeight,
     elemWidth: overlayElemWidth,
     offsetLeft,
-    offsetTop
+    offsetTop,
   };
   let posiInfo!: PositionReturn;
   const _position: string[] = [];
   // const left = getLeft(offsetInfo);
-  if (position.indexOf('top') !== -1) {
+  if (position.indexOf("top") !== -1) {
     posiInfo = getTop({ ...offsetInfo, offset: 3 });
-  } else if (position.indexOf('bottom') !== -1) {
+  } else if (position.indexOf("bottom") !== -1) {
     posiInfo = getBottom({ ...offsetInfo, offset: 3 });
   }
-  if (position.indexOf('right') !== -1) {
+  if (position.indexOf("right") !== -1) {
     posiInfo.left = offsetLeft + offsetWidth - overlayElemWidth;
-    _position.push('right');
+    _position.push("right");
   } else {
-    _position.push('left');
+    _position.push("left");
   }
   _position.push(posiInfo.position);
   // overlayElem.classList.add(position);
@@ -109,50 +114,53 @@ const calculateOverlayPosition = (options) => {
   return posiInfo;
 };
 
-export class DropdownWrapper extends React.PureComponent<DropdownWrapperProps, State> {
+export class DropdownWrapper extends React.PureComponent<
+  DropdownWrapperProps,
+  State
+> {
   static defaultProps = {
-    menuTitle: 'Title',
-    trigger: 'click',
+    menuTitle: "Title",
+    trigger: "click",
     scrollX: 0,
     scrollY: 0,
-    position: 'bottom,left',
-    scrollElem: () => document
+    position: "bottom,left",
+    scrollElem: () => document,
   };
 
   state = {
     isShow: false,
     ready: false,
-    searchValue: ''
+    searchValue: "",
   };
 
-  _position
+  _position;
 
-  _input
+  _input;
 
   // 记录是否已经渲染过一次 overlay
-  _shown = false
+  _shown = false;
 
-  __mount = false
+  __mount = false;
 
-  overlayElem
+  overlayElem;
 
-  displayTitleDOM
+  displayTitleDOM;
 
-  delayExec
+  delayExec;
 
-  hideDebounce
+  hideDebounce;
 
-  addScrollListener
+  addScrollListener;
 
   clickAwayRef: any;
 
   updateNodeRef: any;
 
-  _withInput
+  _withInput;
 
-  _outside
+  _outside;
 
-  isMobile
+  isMobile;
 
   // constructor(props) {
   //   super(props);
@@ -166,9 +174,9 @@ export class DropdownWrapper extends React.PureComponent<DropdownWrapperProps, S
     const isMobile = queryIsMobile();
 
     this.isMobile = isMobile;
-    this._withInput = typeof withInput == 'undefined' ? !isMobile : withInput;
-    this._outside = typeof outside == 'undefined' ? isMobile : outside;
-    this._position = positionFilter(position).split(' ');
+    this._withInput = typeof withInput == "undefined" ? !isMobile : withInput;
+    this._outside = typeof outside == "undefined" ? isMobile : outside;
+    this._position = positionFilter(position).split(" ");
     this.__mount = true;
 
     this.setOutSideContainer(isMobile);
@@ -180,11 +188,11 @@ export class DropdownWrapper extends React.PureComponent<DropdownWrapperProps, S
 
   componentWillUnmount = () => {
     this.__mount = false;
-  }
+  };
 
   handleClickAway = () => {
     if (this.state.isShow) this.hide();
-  }
+  };
 
   handleClickMenu = (e, preventDefault = false) => {
     const { scrollElem } = this.props;
@@ -193,13 +201,13 @@ export class DropdownWrapper extends React.PureComponent<DropdownWrapperProps, S
       // const { clientX, clientY } = e;
       if (!this.addScrollListener && scrollElem) {
         const _scrollElem = scrollElem();
-        _scrollElem.addEventListener('scroll', this.hide);
+        _scrollElem.addEventListener("scroll", this.hide);
       }
       this.addScrollListener = true;
     }
     this.showSubMenu();
     this.focusInput();
-  }
+  };
 
   focusInput() {
     this._input && this._input.focus();
@@ -210,54 +218,57 @@ export class DropdownWrapper extends React.PureComponent<DropdownWrapperProps, S
    */
   setOutSideContainer = (isMobile) => {
     if (this._outside && !dropdownContainerDOM) {
-      dropdownContainerDOM = setDOMById(dropdownContainerID, `__dropdown-menu outside`);
+      dropdownContainerDOM = setDOMById(
+        dropdownContainerID,
+        `__dropdown-menu outside`
+      );
     }
-  }
+  };
 
   showSubMenu = (isShow = true) => {
     this.setState({
       isShow: !!isShow,
     });
-  }
+  };
 
   hide = () => {
     const { scrollElem } = this.props;
     if (this.addScrollListener && scrollElem) {
       const elem = scrollElem();
-      elem.removeEventListener('scroll', this.hide);
+      elem.removeEventListener("scroll", this.hide);
       this.addScrollListener = false;
     }
     /** 确保隐藏成功 */
     if (!this.hideDebounce) this.hideDebounce = new DebounceClass();
     this.hideDebounce.exec(this._hide, 50);
-  }
+  };
 
   _hide = () => {
     if (this.__mount) {
       this.setState({
         isShow: false,
-        searchValue: ''
+        searchValue: "",
       });
     }
-  }
+  };
 
   saveInput = (_i) => {
     if (_i) this._input = _i;
-  }
+  };
 
   onSearch = (e) => {
     const val = e.target.value.trim();
     this.setState({
-      searchValue: val
+      searchValue: val,
     });
-  }
+  };
 
   getPropsForOverlay = () => ({
     ...this.state,
     hide: this.hide,
     showSubMenu: this.showSubMenu,
     focusInput: this.focusInput,
-  })
+  });
 
   saveItems = (e) => {
     this.overlayElem = e;
@@ -268,15 +279,15 @@ export class DropdownWrapper extends React.PureComponent<DropdownWrapperProps, S
       target: this.displayTitleDOM,
       position: this._position,
       scrollX,
-      scrollY
+      scrollY,
     });
-  }
+  };
 
   getOverlayDOM = () => {
     const { overlay } = this.props;
     const { isShow } = this.state;
     if (dropdownContainerDOM) {
-      dropdownContainerDOM.classList.toggle('show', isShow);
+      dropdownContainerDOM.classList.toggle("show", isShow);
     }
 
     // const isLeft = position.indexOf('left') !== -1;
@@ -284,16 +295,12 @@ export class DropdownWrapper extends React.PureComponent<DropdownWrapperProps, S
     const overlayClasses = classnames(
       "dropdown-items",
       !this._outside && this._position,
-      isShow && 'show'
+      isShow && "show"
     );
     const transitionKey = isShow ? "opened" : "closed";
     const dropdownCom = (
       <TransitionGroup component={null}>
-        <CSSTransition
-          key={transitionKey}
-          classNames="drop-menu"
-          timeout={200}
-        >
+        <CSSTransition key={transitionKey} classNames="drop-menu" timeout={200}>
           {isShow ? (
             <div
               ref={this._outside ? (e) => this.saveItems(e) : null}
@@ -303,66 +310,65 @@ export class DropdownWrapper extends React.PureComponent<DropdownWrapperProps, S
               {/* <span className="caret" /> */}
               {overlay && overlay(this.getPropsForOverlay())}
             </div>
-          ) : <span />}
+          ) : (
+            <span />
+          )}
         </CSSTransition>
       </TransitionGroup>
     );
 
     return dropdownCom;
-  }
+  };
 
   overlayRender = () => {
-    return this._outside ? (dropdownContainerDOM && ReactDOM.createPortal(
-      this.getOverlayDOM(),
-      dropdownContainerDOM,
-    )) : this.getOverlayDOM();
-  }
+    return this._outside
+      ? dropdownContainerDOM &&
+          ReactDOM.createPortal(this.getOverlayDOM(), dropdownContainerDOM)
+      : this.getOverlayDOM();
+  };
 
   saveClickAway = (e) => {
     this.clickAwayRef = e;
     if (e) {
       this.updateNodeRef = e.updateNodeRef;
     }
-  }
+  };
 
   getDefaultChild = (menuTitle) => {
     const { searchValue } = this.state;
     return (
       <div className="display-menu">
-        <div className="display-title">
-          {menuTitle}
-        </div>
-        {
-          this._withInput && (
-            <input type="text"
-              ref={this.saveInput}
-              placeholder={typeof menuTitle === 'string' ? menuTitle : ''}
-              value={searchValue}
-              className="search-input"
-              onChange={this.onSearch}
-            />
-          )
-        }
+        <div className="display-title">{menuTitle}</div>
+        {this._withInput && (
+          <input
+            type="text"
+            ref={this.saveInput}
+            placeholder={typeof menuTitle === "string" ? menuTitle : ""}
+            value={searchValue}
+            className="search-input"
+            onChange={this.onSearch}
+          />
+        )}
         <div className="icon-wrap">
           <Icon n="angle-down" />
         </div>
       </div>
     );
-  }
+  };
 
   childrenRender = () => {
     const { children, menuTitle } = this.props;
     let child;
     let _title = menuTitle;
     switch (true) {
-      case typeof children === 'function':
+      case typeof children === "function":
         child = children(this.getPropsForOverlay());
         break;
       case React.isValidElement(children):
         child = children;
         break;
       default:
-        if (typeof children === 'string') {
+        if (typeof children === "string") {
           _title = children;
         }
         child = this.getDefaultChild(_title);
@@ -374,85 +380,82 @@ export class DropdownWrapper extends React.PureComponent<DropdownWrapperProps, S
     // return React.cloneElement(child, {
     //   onMouseEnter: this.setOutSideContainer
     // });
-  }
+  };
 
   handleMouseEnter = (event) => {
     if (this.delayExec) this.delayExec.cancel();
     this.handleClickMenu(event);
-  }
+  };
 
   handleMouseLeave = () => {
     if (!this.delayExec) this.delayExec = new DebounceClass();
     this.delayExec.exec(this.handleClickAway, 100);
-  }
+  };
 
   bindOverlayTrigger = () => {
     const { trigger } = this.props;
     let res = {};
     switch (trigger) {
-      case 'click':
+      case "click":
         if (this._outside) {
           res = {
-            onClick: (e) => this.handleClickMenu(e, true)
+            onClick: (e) => this.handleClickMenu(e, true),
           };
         }
         break;
-      case 'hover':
+      case "hover":
         res = {
           onMouseEnter: this.handleMouseEnter,
-          onMouseLeave: this.handleMouseLeave
+          onMouseLeave: this.handleMouseLeave,
         };
         break;
     }
     return res;
-  }
+  };
 
   bindWrapperTrigger = () => {
     const { trigger } = this.props;
     let res = {};
     switch (trigger) {
-      case 'click':
+      case "click":
         res = {
-          onClick: this.handleClickMenu
+          onClick: this.handleClickMenu,
         };
         break;
-      case 'hover':
+      case "hover":
         res = {
           onMouseEnter: this.handleMouseEnter,
-          onMouseLeave: this.handleMouseLeave
+          onMouseLeave: this.handleMouseLeave,
         };
         break;
     }
     return res;
-  }
+  };
 
   render() {
     const { isShow, ready } = this.state;
-    const {
-      className, style, error
-    } = this.props;
+    const { className, style, error } = this.props;
 
     const classNames = classnames(
       "__dropdown-menu",
       className,
       this._withInput && "input-mode",
-      error && 'error',
-      isShow && 'show',
+      error && "error",
+      isShow && "show",
       !this._outside && this._position
     );
 
     return (
       <ClickAway ref={this.saveClickAway} onClickAway={this.handleClickAway}>
-        <div
-          className={classNames}
-          style={style}
-        >
-          <div className="menu-wrapper" ref={(e) => { this.displayTitleDOM = e; }}
+        <div className={classNames} style={style}>
+          <div
+            className="menu-wrapper"
+            ref={(e) => {
+              this.displayTitleDOM = e;
+            }}
             {...this.bindWrapperTrigger()}
           >
-            {
-              this.childrenRender()
-            }
+            {this.childrenRender()}
           </div>
           {this.overlayRender()}
         </div>
